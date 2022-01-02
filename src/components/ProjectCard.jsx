5a1a0ee821fba113d5859980866fd3c64cc2944a
Portfolio <jpy20@outlook.com>
2022-01-02 00:53:39 -0600

    Second page completed, missing responsive design

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,11 @@
+import './ProjectCard.css'
+
+function ProjectCard() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default ProjectCard;
--- src/components/ThirdPage.jsx
+++ src/components/ThirdPage.jsx
@@ -1,9 +1,13 @@
 import './ThirdPage.css'
-
+import ProjectCard from './ProjectCard';
 function ThirdPage() {
     return(
         <div className="tp-main">
-
+            <div className='third-page-information'>
+                <h1>My Work</h1>
+                <p>A small selection of outstanding projects that I have worked on.</p>
+            </div>
+            <ProjectCard/>
         </div>
     );
 }
