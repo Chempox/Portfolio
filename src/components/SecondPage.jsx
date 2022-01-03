913b7550394b85b5af767aca7241a84f8b445197
Portfolio <jpy20@outlook.com>
2022-01-03 01:41:40 -0600

    Animations at work

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -1,13 +1,18 @@
 
 import './SecondPage.css'
+import { motion } from "framer-motion"
 
 function SecondPage() {
     return (
     <div className='sp-container'>
             <div className='about-me'>
-                <div className='second-page-information'>
+                <motion.div className='second-page-information'
+                initial={{opacity: 0}}
+                animate={{opacity: 100}}
+                transition={{duration: 25}}
+                >
                     <img src='https://media-exp1.licdn.com/dms/image/C5603AQG2BXKFFrWsdw/profile-displayphoto-shrink_800_800/0/1625810093742?e=1645660800&v=beta&t=ZSLVuo58lTzUqcdTc-_ELuk408q8IZs9dRBcqm-0kVE' alt="asdasdasd"/>
-                    <p>I'm a Full Stack web developer located in Mexico. Focus on building accesible, unique, and intuitive web/movile aplications.<br/><br/>I have been programming since 2018. Throughout this years I have participated in different projects and worked with several technologies and languages.<br/><br/>I'm currently working as a web developer and about to finish my degree in computer science, in addition to that I'm working on ambitious projects with amazing people.</p>
+                    <p>I'm a Full Stack web developer located in Mexico. Focus on building accesible, unique, and intuitive web/mobile aplications.<br/><br/>I have been programming since 2018. Throughout this years I have participated in different projects and worked with several technologies and languages.<br/><br/>I'm currently working as a web developer and about to finish my degree in computer science, in addition to that I'm working on ambitious projects with amazing people.</p>
                     <h2>Technologies I have recently worked with:</h2>
                     <div className='technologies'>
                         <h3>⮞ Node.js</h3>
@@ -17,9 +22,20 @@ function SecondPage() {
                         <h3>⮞ Kotlin</h3>
                         <h3>⮞ C++</h3>
                     </div>
-                </div>
-                <div className='about-me-header-1'><h1>About <span>M</span>e</h1></div>
-                <div className='about-me-header-2'><h1>About M<span>e</span></h1></div>
+                </motion.div>
+                <motion.div className='about-me-header-1'
+                initial={{x: '-100%'}}
+                animate={{x: 0}}
+                transition={{duration: 1}}
+                >
+                <h1>About Me</h1>
+                </motion.div>
+                <motion.div className='about-me-header-2'
+                initial={{x: '100%'}}
+                animate={{x: 0}}
+                transition={{duration: 1}}>
+                    <h1>About Me</h1>
+                </motion.div>
             </div>
     </div>
     );
--- src/components/ThirdPage.jsx
+++ src/components/ThirdPage.jsx
@@ -1,25 +1,82 @@
 import './ThirdPage.css'
 import ProjectCardLeft from './ProjectCardLeft';
 import ProjectCardRight from './ProjectCardRight';
+import { motion } from "framer-motion";
+import {useInView} from 'react-intersection-observer';
+import {useEffect} from 'react';
+import {useAnimation} from 'framer-motion';
 
 function ThirdPage() {
+
+    const {ref, inView} = useInView();
+    const {refTwo, inViews} = useInView();
+    const animation = useAnimation();
+    const animationTwo = useAnimation();
+    
+    useEffect(() =>{
+        if(inView)
+        {
+            animation.start({
+                x: 0,
+                transition: {
+                    duration: 1
+                }
+            })
+        }
+
+        if(!inView)
+        {
+            animation.start({x: '-100vw'})
+        }
+    },[inView])
+
+
+    useEffect(() =>{
+        if(inViews)
+        {
+            animationTwo.start({
+                x: 0,
+                transition: {
+                    duration: 1
+                }
+            })
+        }
+
+        if(!inViews)
+        {
+            animationTwo.start({x: '-100vw'})
+        }
+    },[inViews])
+
     return(
         <div className="tp-main">
-            <div className='third-page-information'>
-                <h1>My Work</h1>
+            <motion.div className='third-page-information'
+            initial={{x: '100%'}}
+            animate={{x: 0}}
+            transition={{duration: 1}}
+            >
+                <h1>MY WORK</h1>
                 <p>A small selection of projects that I have worked on.</p>
-            </div>
+            </motion.div>
             <br />
             <div className='tp-cards'>
-                <div className='tp-content-left'>
+                <motion.div ref={ref} className='tp-content-left'
+                animate={animation}
+                >
                     <ProjectCardLeft/>
-                </div>
-                <div className='tp-content-right'>
+                </motion.div>
+                <motion.div className='tp-content-right'
+                initial={{x: '100%'}}
+                animate={{x: 0}}
+                transition={{duration: 1}}
+                >
                     <ProjectCardRight/>
-                </div>
-                <div className='tp-content-left'>
+                </motion.div>
+                <motion.div ref={refTwo} className='tp-content-left'
+                animate={animationTwo}
+                >
                     <ProjectCardLeft/>
-                </div>
+                </motion.div>
             </div>
         </div>
     );
