731aef4c63f675355a73cf7d334eb2e6280ab1b1
Portfolio <jpy20@outlook.com>
2022-01-04 22:13:18 -0600

    Update commit

--- src/components/FourthPage.jsx
+++ src/components/FourthPage.jsx
@@ -1,25 +1,6 @@
 import './FourthPage.css'
 import FourthPageCard from './FourthPageCard';
 function FourthPage () {
-    const {ref, inView} = useInView(
-        );
-        const animation = useAnimation();
-        useEffect(() =>{
-            if(inView)
-            {
-                animation.start({
-                    x: 0,
-                    transition: {
-                        type: 'spring',duration: 1, bounce: 0.3
-                    }
-                })
-            }
-    
-            if(!inView)
-            {
-                animation.start({x: '100%'})
-            }
-        })
     return(
         <div className="fp-container">
             <FourthPageCard/>
